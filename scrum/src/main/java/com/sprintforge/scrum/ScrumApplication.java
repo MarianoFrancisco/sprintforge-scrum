f2d2f7a208432dcc7128e85f4915be0c0786c9c5
Mariano Camposeco <mariano1941@outlook.es>
2025-12-17 16:48:25 -0600

    feat: add kafka configuration

--- scrum/src/main/java/com/sprintforge/scrum/ScrumApplication.java
+++ scrum/src/main/java/com/sprintforge/scrum/ScrumApplication.java
@@ -2,11 +2,13 @@ package com.sprintforge.scrum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.sprintforge")
+@ConfigurationPropertiesScan(basePackages = "com.sprintforge")
 public class ScrumApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(ScrumApplication.class, args);
 	}
 
