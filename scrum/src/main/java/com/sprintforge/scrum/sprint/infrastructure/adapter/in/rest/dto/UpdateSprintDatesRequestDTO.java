db2b3f1680d9e90e378128e5b1ffb937e1affc74
Mariano Camposeco <mariano1941@outlook.es>
2025-12-27 22:08:49 -0600

    refactor: change sprint dto

--- scrum/src/main/java/com/sprintforge/scrum/sprint/infrastructure/adapter/in/rest/dto/UpdateSprintDatesRequestDTO.java
+++ scrum/src/main/java/com/sprintforge/scrum/sprint/infrastructure/adapter/in/rest/dto/UpdateSprintDatesRequestDTO.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotNull;
 import java.time.Instant;
 import java.util.UUID;
 
-@END_AFTER_START(start = "startDate", end = "endDate", message = "endDate debe ser posterior a startDate")
+@END_AFTER_START(start = "startDate", end = "endDate", message = "La fecha de fin debe ser posterior a la fecha de inicio")
 public record UpdateSprintDatesRequestDTO(
         @NotNull(message = "El empleado que realiza la acción es obligatorio")
         UUID employeeId,
