b9e13860b72a6e0a0e932fc3219e9267973ed102
Mariano Camposeco <mariano1941@outlook.es>
2025-12-27 22:05:48 -0600

    feat: add github workflow

--- scrum/src/main/java/com/sprintforge/scrum/payment/application/service/MakePaymentImpl.java
+++ scrum/src/main/java/com/sprintforge/scrum/payment/application/service/MakePaymentImpl.java
@@ -15,7 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 @Transactional(rollbackFor = Exception.class)
-public class CreatePaymentImpl implements CreatePayment {
+public class PaymentMadeImpl implements CreatePayment {
 
     private final GetProjectById getProjectById;
     private final SavePayment savePayment;
