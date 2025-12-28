3ff89d8028295328b3c6f3462d7a2fafaee511de
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-12-27 21:43:41 -0600

    fix: close and open project command variables order

--- scrum/src/main/java/com/sprintforge/scrum/project/application/port/in/command/CloseProjectCommand.java
+++ scrum/src/main/java/com/sprintforge/scrum/project/application/port/in/command/CloseProjectCommand.java
@@ -3,7 +3,7 @@ package com.sprintforge.scrum.project.application.port.in.command;
 import java.util.UUID;
 
 public record CloseProjectCommand(
-        UUID employeeId,
-        UUID id
+        UUID id,
+        UUID employeeId
 ) {
 }
--- scrum/src/main/java/com/sprintforge/scrum/project/application/port/in/command/OpenProjectCommand.java
+++ scrum/src/main/java/com/sprintforge/scrum/project/application/port/in/command/OpenProjectCommand.java
@@ -3,7 +3,7 @@ package com.sprintforge.scrum.project.application.port.in.command;
 import java.util.UUID;
 
 public record OpenProjectCommand(
-        UUID employeeId,
-        UUID id
+        UUID id,
+        UUID employeeId
 ) {
 }
